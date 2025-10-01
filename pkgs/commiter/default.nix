9824428546a333fa48c4df79a5eadb1d6b94535c
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-10-01 11:52:33 -0400

    update version to 0.2.1

--- pkgs/commiter/default.nix
+++ pkgs/commiter/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "commiter";
-  version = "0.2.0";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "lmdevv";
     repo = "commiter";
-    rev = "046225ba61616b055572d3d80fc78c600a68d45b";
-    sha256 = "0zdsnc06923rzkzwmk5p12x7dki7px2agyby3daiibv227qmwnwc";
+    rev = "9530ae64759493731e8c5edff9b03e968bb1fc71";
+    sha256 = "07y78xfia7l7shfmg1ar7n4fab39hrf2b1vdq9dnfpizl23c1frn";
   };
 
   vendorHash = null;
