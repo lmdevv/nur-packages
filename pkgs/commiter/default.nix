0a54ac1261a84f6a5c4f83f48d157e90f70f0df0
lmdev <138343225+lmdevv@users.noreply.github.com>
2025-10-16 20:59:21 -0400

    update commiter to c66e0d6

--- pkgs/commiter/default.nix
+++ pkgs/commiter/default.nix
@@ -11,8 +11,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "lmdevv";
     repo = "commiter";
-    rev = "2b5a65d4934e40d289c1156b9b7e7288d2501140";
-    sha256 = "1c3j9s366b766pbzglp02r05axfd086d2vn7jra28kzjmfd5aygg";
+    rev = "c66e0d621412975396517763fcefe10ae3116e96";
+    sha256 = "04fl7jfr8zapcz83sj4y4jqcmn9sgh2zqkq5vmy140knyd0jcxm2";
   };
 
   vendorHash = null;
